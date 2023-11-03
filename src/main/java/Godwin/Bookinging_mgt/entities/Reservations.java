510acb9dd976929387ddb3184199465e52c36487
Godwin <godwinmail6@gmail.com>
2023-11-03 19:08:33 +0100

    Reservation class added

--- src/main/java/Godwin/Bookinging_mgt/entities/Reservations.java
+++ src/main/java/Godwin/Bookinging_mgt/entities/Reservations.java
@@ -6,21 +6,24 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 @Entity
-@Table(name = "work_station")
+@Table(name = "reservations")
 @Getter
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
-public class WorkStation {
+public class Reservations {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false)
     private long id;
-    private String description;
-    private Type type;
-    private long maxOccupants;
-
+    private String name;
+    private String surname;
+    private String email;
+    private LocalTime reservationTime;
 
 }
