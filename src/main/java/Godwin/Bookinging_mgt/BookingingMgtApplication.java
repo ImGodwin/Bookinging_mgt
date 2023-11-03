df5fe0007b04fdedf0b167b58812a7e0b972d5a2
Godwin <godwinmail6@gmail.com>
2023-11-03 18:22:14 +0100

    First commit

---
+++ src/main/java/Godwin/Bookinging_mgt/BookingingMgtApplication.java
@@ -0,0 +1,15 @@
+package Godwin.Bookinging_mgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingingMgtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingingMgtApplication.class, args);
+
+		System.out.println("<<<<<<<<<<<<<<<<<<<<<hello");
+	}
+
+}
---
+++ src/test/java/Godwin/Bookinging_mgt/BookingingMgtApplicationTests.java
@@ -0,0 +1,13 @@
+package Godwin.Bookinging_mgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingingMgtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
